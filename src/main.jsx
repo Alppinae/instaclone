e896b5323342d90c4ec7bf408ed7f9fa473e7d5e
Alppinae <seferalp22@gmail.com>
2024-06-29 12:17:01 +0300

    güncelleme

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {ThemeProvider, createTheme} from '@material-ui/core/styles'
+import {ThemeProvider, createTheme} from '@mui/material'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
